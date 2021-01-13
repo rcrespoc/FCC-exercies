f2df4322c0904b657adbad63d95d2c0bd61d719b
Richard <rjcrespo96@gmail.com>
2021-01-13 00:34:58 -0500

    Nuevos ejercicios

---
+++ freeCodeCamp/basic-algorithm/truncate-string.js
@@ -0,0 +1,7 @@
+/* Truncate a string (first argument) if it is 
+longer than the given maximum string length 
+(second argument). Return the truncated string 
+with a ... ending. */
+
+const truncateString = (str, num) =>
+  num >= str.length ? str : str.slice(0, num) + "...";
